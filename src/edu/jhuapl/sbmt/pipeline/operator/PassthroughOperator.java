16b9495fec268bb244703728b246118d31805d30
Josh Steele <josh.steele@jhuapl.edu>
2024-08-23 16:16:52 -0400

    Addresses #2 - added some docs, removed old class

--- src/edu/jhuapl/sbmt/pipeline/operator/PassthroughOperator.java
+++ src/edu/jhuapl/sbmt/pipeline/operator/PassthroughOperator.java
@@ -2,8 +2,17 @@ package edu.jhuapl.sbmt.pipeline.operator;
 
 import java.io.IOException;
 
+/**
+ * A no-op operator which simply passes data in with no changes. This can be used in a conditional where an operator may be required
+ * and if not, the data can simply be passed through with this operator.
+ * 
+ * @param <T1>
+ */
 public class PassthroughOperator<T1> extends BasePipelineOperator<T1, T1>
 {
+	/**
+	 * Default no-arg constructor
+	 */
 	public PassthroughOperator()
 	{
 
